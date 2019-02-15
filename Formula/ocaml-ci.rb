fb5e8e1cc46fc59085e02f99e61226462fae7665
Anil Madhavapeddy <anil@recoil.org>
2019-02-15 07:47:45 -0800

    add experimental ocaml-ci formula

---
+++ Formula/ocaml-ci.rb
@@ -0,0 +1,12 @@
+class Ocamlci < Formula
+  desc "The OCaml CI environment" 
+  homepage "https://ocaml.org"
+
+  depends_on "opam"
+  depends_on "m4" => :build
+
+  def install
+    system "echo", "opam", "init", "-n", "--bare", "--root", "#{prefix}"
+
+  end
+end
