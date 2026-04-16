ec2f85769dd8c413c452e8bd48d5a8a420d62c18
Anil Madhavapeddy <anil@recoil.org>
2026-04-16 17:00:17 +0100

    +oi

---
+++ Formula/oi.rb
@@ -0,0 +1,17 @@
+class Oi < Formula
+  desc "Oi is a stateless OCaml installer"
+  homepage "https://github.com/avsm/oi/"
+  url "https://github.com/avsm/oi.git", branch: "main"
+  version "0.0.1"
+  license "ISC"
+
+  depends_on "gpatch"
+  depends_on "opam"
+  depends_on "sqlite"
+  depends_on "zstd"
+
+  def install
+    system "bash", "./.opambuild.sh"
+    bin.install "_opam/bin/oi"
+  end
+end
