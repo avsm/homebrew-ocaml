dbac20f6adc83d78bb41703658e37e6932d7a436
Anil Madhavapeddy <anil@recoil.org>
2026-05-01 15:57:32 +0000

    oi: add 0.8.1 bottle.

--- Formula/oi.rb
+++ Formula/oi.rb
@@ -4,6 +4,12 @@ class Oi < Formula
   url "https://github.com/avsm/oi.git", tag: "0.8.1"
   license "ISC"
 
+  bottle do
+    root_url "https://github.com/avsm/homebrew-ocaml/releases/download/oi-0.8.1"
+    sha256 cellar: :any, arm64_sequoia: "db841a46cebe66938942dce45dd27871b07982108e63b6c6cc94d5fdbfcf5e23"
+    sha256               x86_64_linux:  "d9b4e9e7c784ca72502e262c7342edd31ea2d6cc9f9454a8d27ad92c6b10dd65"
+  end
+
   depends_on "gpatch"
   depends_on "opam"
   depends_on "pkgconf"
