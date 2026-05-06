903a39265d1769ec3ee8b47bb306f2bd569bb8d3
Anil Madhavapeddy <anil@recoil.org>
2026-05-06 07:38:20 +0100

    oi 0.9.8

--- Formula/oi.rb
+++ Formula/oi.rb
@@ -1,14 +1,8 @@
 class Oi < Formula
   desc "Stateless OCaml installer and development manager"
   homepage "https://github.com/avsm/oi/"
-  url "https://github.com/avsm/oi.git", tag: "0.9.5"
+  url "https://github.com/avsm/oi.git", tag: "0.9.8"
   license "ISC"
-
-  bottle do
-    root_url "https://github.com/avsm/homebrew-ocaml/releases/download/oi-0.9.5"
-    sha256 arm64_sequoia: "0f99a2fa48af7453f34386c4e9ee4ebf5202aec8d083e184aaadd671ec3eca12"
-    sha256 x86_64_linux:  "4dafb29b2ffedd298a9f4059d156a92553c9493b7c099f835450a5fedf78116d"
-  end
 
   depends_on "gmp"
   depends_on "gpatch"
