abf75e148af2d3f63af1d166f6cfc1279f2d6932
Anil Madhavapeddy <anil@recoil.org>
2026-05-13 14:17:02 +0100

    +0.11.1
    Closes #20.
    
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

--- Formula/oi.rb
+++ Formula/oi.rb
@@ -1,14 +1,8 @@
 class Oi < Formula
   desc "Stateless OCaml installer and development manager"
   homepage "https://github.com/avsm/oi/"
-  url "https://github.com/avsm/oi.git", tag: "0.11.0"
+  url "https://github.com/avsm/oi.git", tag: "0.11.1"
   license "ISC"
-
-  bottle do
-    root_url "https://github.com/avsm/homebrew-ocaml/releases/download/oi-0.11.0"
-    sha256 arm64_sequoia: "18c8a083fec8b08062ee7efb14310f91d12655286dc7c7cb9900484b8874efe9"
-    sha256 x86_64_linux:  "3a42732b7bd9d9ab84b4d421636ff38ff9e2fe60d7ae5b7021f52d0aebbdc208"
-  end
 
   depends_on "gmp"
   depends_on "gpatch"
